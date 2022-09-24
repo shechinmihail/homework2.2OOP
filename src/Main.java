03a19ae92e2bb471d5ceb1f0d654bdc14a47e85e
shechin <shechinmihail@mail.ru>
2022-09-25 00:43:08 +0500

    Выполнил самое последнее задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human Maksim = new Human("Maksim");
-        Maksim.town = "Minsk";
-        Maksim.yearOfBirth = 2022 - 35;
-        Maksim.jobTitle = "бренд-менеджер";
+        Human Maksim = new Human(2022-35, "Maksim", "Minsk", "бренд-менеджер");
+        Human Anna = new Human(2022-29, "Anna", "Moscow", "методистом образовательных программ");
+        Human Katya = new Human(2022-28, "Katya", "Kaliningrad", "продакт-менеджер");
+        Human Artem = new Human(2022-27, "Artem", "Moscow", "директором по развитию бизнеса");
+
         Maksim.greetings();
-
-
-        Human Anna = new Human("Anna");
-        Anna.town = "Moscow";
-        Anna.yearOfBirth = 2022 - 29;
-        Anna.jobTitle = "методистом образовательных программ";
         Anna.greetings();
-
-        Human Katya = new Human("Katya");
-        Katya.town = "Kaliningrad";
-        Katya.yearOfBirth = 2022 - 28;
-        Katya.jobTitle = "продакт-менеджер";
         Katya.greetings();
-
-        Human Artem = new Human("Artem");
-        Artem.town = "Moscow";
-        Artem.yearOfBirth = 2022 - 27;
-        Artem.jobTitle = "директором по развитию бизнеса";
         Artem.greetings();
-
     }
 }
--- src/Task3/Car.java
+++ src/Task3/Car.java
@@ -10,12 +10,212 @@ public class Car {
     String productionCountry;
 
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+    }
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear) {
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Car(String brand, String model, double engineVolume, String color) {
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Car(String brand, String model, double engineVolume) {
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Car(String brand, String model) {
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+    }
+
+    public Car(String brand) {
+        if (brand == null) {
+            this.brand =
+                    "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
     }
 
     @Override
@@ -29,3 +229,5 @@ public class Car {
                 ", страна сборки " + productionCountry;
     }
 }
+
+
